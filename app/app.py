f4f6e80029df42ae8f37833daef618499ad6eeeb
MuNeNiCK <mune0127@munenick.me>
2022-10-04 15:27:20 +0900

    add test

--- app/app.py
+++ app/app.py
@@ -1,6 +1,13 @@
 from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+from flask_login import LoginManager, login_user
+
 
 app = Flask(__name__)
+
+app.config.from_object('config')
+
+import views
 
 if __name__ == '__main__':
     app.run(host='localhost')
